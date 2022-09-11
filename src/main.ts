41be7384fc2949e20c3532688b5dacc4580d5769
Bruno Lira <bruno.lira@dotz.com>
2022-09-11 16:40:01 -0300

    chore(appPort): update port

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const PORT = process.env.PORT ?? 3000;
+
+  await app.listen(PORT, () => {
+    console.log(`server listening at http://localhost:${PORT}`);
+  });
 }
 bootstrap();
